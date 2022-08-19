d15f808e9fae8f3af61923fee743db5a25b02023
1264932975 <1264932975@qq.com>
2022-08-19 15:20:55 +0800

    a

--- src/main/java/com/example/demo/oula/St11Prime007.java
+++ src/main/java/com/example/demo/oula/St11Prime007.java
@@ -16,11 +16,8 @@ public class St11Prime007 {
         while (num != 10001) {
             temp += 2;//双数都能被2整除，一次跳过两个数
             boolean b = true;
-            for (int i = 3; i < temp / 2; i += 2) {//i超过temp一半后均为前一半的倍数，可以直接跳过
-                if (temp / 2 != 0 && temp % 2 == 0) {//能被2整除则能被2的倍数整除，只判断2，其余直接跳过
-                    b = false;
-                    break;
-                } else if (temp / i != 0 && temp % i == 0) {//是否被整除
+            for (int i = 3; i*i <= temp ; i += 2) {
+                if (temp / i != 0 && temp % i == 0) {//是否被整除
                     b = false;
                     break;
                 }
