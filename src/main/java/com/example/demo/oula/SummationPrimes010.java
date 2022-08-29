683b43725c4a52208a36ce9b0712b24c07d84663
00深 <1264932975@qq.com>
2022-08-29 13:12:59 +0800

    a

--- src/main/java/com/example/demo/oula/SummationPrimes010.java
+++ src/main/java/com/example/demo/oula/SummationPrimes010.java
@@ -2,31 +2,32 @@ package com.example.demo.oula;
 
 import java.util.Date;
 
-public class St11Prime007 {
+public class SummationPrimes010 {
     public static void main(String[] args) {
-        System.out.println(st11Prime007());
+        System.out.println(summationPrimes010());
 
     }
 
-    public static String st11Prime007() {
+    public static String summationPrimes010() {
         Date st = new Date();
-        int num = 6;
-        int temp = 15;
-        while (num != 10001) {
+        Long sum = 5l;
+        int temp = 3;
+        while (temp < 2000000) {
             temp += 2;//双数都能被2整除，一次跳过两个数
             boolean b = true;
-            for (int i = 3; i*i <= temp ; i += 2) {
-                if (temp % i == 0 && temp / i != 0 ) {//是否被整除
+            for (int i = 3; i * i <= temp; i += 2) {
+                if (temp % i == 0 && temp / i != 0) {//是否被整除
                     b = false;
                     break;
                 }
             }
             if (b) {
-                num++;
+                sum = sum + temp;
             }
         }
         Date end = new Date();
         return String.format("运行时间:%s ms\n答案：%s",
-                end.getTime() - st.getTime(), temp);
+                end.getTime() - st.getTime(), sum);
     }
+
 }
